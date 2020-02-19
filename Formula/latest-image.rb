45d3630c4d994df7ab697f0ddc7fc625e50522cc
Zachery Bir <zbir@zacbir.net>
2020-02-19 10:49:55 -0500

    Add to-prod and fix sha sum for latest-image

--- Formula/latest-image.rb
+++ Formula/latest-image.rb
@@ -5,12 +5,12 @@ class LatestImage < Formula
   
     if OS.mac?
       url "https://binaries.ddbuild.io/devtools/pex/latest-image/latest-image-#{version}.pex", :using => :nounzip
-      sha256 "8866665bd96c6ecb4f2c5ee03738aa6e0962ef6d348feaeeb7403828051fb2bc"
+      sha256 "373a483ddc13faf864b9ac11b3ec1e0ff4282837edde4512d004682b8bee7769"
     end
   
     def install
       # Homebrew downloads the ddtool binary as `binaries.ddbuild.io`
-      system "cp", "latest-image-#{version}.pex", "latest-imageb"
+      system "cp", "latest-image-#{version}.pex", "latest-image"
       bin.install "latest-image"
     end
   end
---
+++ Formula/to-prod.rb
@@ -0,0 +1,16 @@
+class ToProd < Formula
+    desc "to-prod deploys a particular branch or tag to production environments"
+    homepage "https://github.com/datadog/devtools/"
+    version "unversioned"
+  
+    if OS.mac?
+      url "https://binaries.ddbuild.io/devtools/pex/to-prod/to-prod-#{version}.pex", :using => :nounzip
+      sha256 "3d597e7c3c7a53d28eebe60870b24fc148a24cc42f8dab9375333daa35f17e16"
+    end
+  
+    def install
+      # Homebrew downloads the ddtool binary as `binaries.ddbuild.io`
+      system "cp", "to-prod-#{version}.pex", "to-prod"
+      bin.install "to-prod"
+    end
+  end
