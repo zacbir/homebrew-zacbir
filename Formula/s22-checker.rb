d5c4ce702a9f09c1dec4872006a23d0c38d6a116
Zachery Bir <zbir@zacbir.net>
2020-02-20 14:29:08 -0500

    New SHA, new version

--- Formula/s22-checker.rb
+++ Formula/s22-checker.rb
@@ -1,11 +1,11 @@
 class S22Checker < Formula
     desc "s22-checker is a command line tool for determining if secrets have been added to repos."
     homepage "https://github.com/datadog/devtools/"
-    version "new-unversioned"
+    version "v1-1"
   
     if OS.mac?
       url "https://binaries.ddbuild.io/devtools/pex/s22-checker/s22-checker-#{version}.pex", :using => :nounzip
-      sha256 "8512774814a7eabbecb613033fbd546f97f244d5409b422a61e6a72fb12b0892"
+      sha256 "85ed5612d9147196cf6fce5de77c31b131e64288251b0ba7c841c7f482f8b122"
     end
   
     def install
